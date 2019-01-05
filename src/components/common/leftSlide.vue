92646f822b0c3abecad404bb3051de98a4919c0d
sjhty <sun_j1986@163.com>
2019-01-05 22:08:27 +0800

    add

---
+++ src/components/common/leftSlide.vue
@@ -0,0 +1,17 @@
+<template>
+    <el-aside width="200px" style="background-color: rgb(238, 241, 246)">
+        <el-menu :default-openeds="['1', '3']">
+        <el-submenu index="1">
+            <template slot="title"><i class="el-icon-message"></i>用户管理</template>
+            <el-menu-item index="2-1">用户列表</el-menu-item>
+            <el-menu-item index="2-2">创建用户</el-menu-item>
+        </el-submenu>
+        <el-submenu index="2">
+            <template slot="title"><i class="el-icon-menu"></i>商品管理</template>
+        </el-submenu>
+        <el-submenu index="3">
+            <template slot="title"><i class="el-icon-setting"></i>库存管理</template>
+        </el-submenu>
+        </el-menu>
+    </el-aside>
+</template>
---
+++ src/components/common/manage.vue
@@ -0,0 +1,59 @@
+<template>
+    <el-container style="height: 800px; border: 1px solid #eee">
+        <el-header style="text-align: right; font-size: 12px">
+            <el-dropdown>
+                <i class="el-icon-setting" style="margin-right: 15px"></i>
+                <el-dropdown-menu slot="dropdown">
+                <el-dropdown-item>查看</el-dropdown-item>
+                <el-dropdown-item>新增</el-dropdown-item>
+                <el-dropdown-item>删除</el-dropdown-item>
+                </el-dropdown-menu>
+            </el-dropdown>
+            <span>王小虎</span>
+        </el-header>
+        <el-container>
+            <leftSlide></leftSlide>
+            <el-main>
+                <el-table :data="tableData">
+                    <el-table-column prop="date" label="日期" width="140">
+                    </el-table-column>
+                    <el-table-column prop="name" label="姓名" width="120">
+                    </el-table-column>
+                    <el-table-column prop="address" label="地址">
+                    </el-table-column>
+                </el-table>
+            </el-main>
+        </el-container>
+    </el-container>
+</template>
+
+<style>
+  .el-header {
+    background-color: rgb(238, 241, 246);
+    color: #333;
+    line-height: 60px;
+  }
+  
+  .el-aside {
+    color: #333;
+  }
+</style>
+
+<script>
+  import leftSlide from "./leftSlide";
+  export default {
+    data() {
+      const item = {
+        date: '2016-05-02',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1518 弄'
+      };
+      return {
+        tableData: Array(20).fill(item)
+      }
+    },
+
+    components: {leftSlide}
+
+  };
+</script>
