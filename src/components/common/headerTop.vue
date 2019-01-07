3ffab71e59c1b493a4cd93923e52559bec302ed2
sjhty <sun_j@1986@163.com>
2019-01-07 17:30:15 +0800

    master

--- src/components/common/headerTop.vue
+++ src/components/common/headerTop.vue
@@ -1,14 +1,6 @@
 <template>
+    <el-col class="el-sm-3"><img src="@/assets/logo.png"></el-col>
     
-    <el-dropdown>
-        <i class="el-icon-setting" style="margin-right: 15px"></i>
-        <el-dropdown-menu slot="dropdown">
-        <el-dropdown-item>查看</el-dropdown-item>
-        <el-dropdown-item>新增</el-dropdown-item>
-        <el-dropdown-item>删除</el-dropdown-item>
-        </el-dropdown-menu>
-    </el-dropdown>
-    <!-- <span>王小虎</span> -->
 </template>
 <style>
 
